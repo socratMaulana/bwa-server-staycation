f42e0ee930a1b4590987c7c7621338f77044e82e
socratMaulana <97397511+socratMaulana@users.noreply.github.com>
2022-03-14 19:35:43 +0700

    change

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const flash = require('connect-flash');
 
 //import mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/db_staycation', {
+mongoose.connect('mongodb+srv://user:bwamern@cluster0.pil1c.mongodb.net/db_staycation?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
