abfdef4828596b691cd8c7eaa8536fba83f25fd9
socratMaulana <97397511+socratMaulana@users.noreply.github.com>
2022-03-29 20:19:09 +0700

    edited

--- app.js
+++ app.js
@@ -3,41 +3,42 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
-
-
-//import mongoose
+const cors = require('cors');
+// import mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://user:bwamern@cluster0.pil1c.mongodb.net/db_staycation?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-}); 
+mongoose.connect('mongodb+srv://codeathome:bwamern@cluster0-40j6e.mongodb.net/db_staycation?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-//router admin
+// router admin
 const adminRouter = require('./routes/admin');
 const apiRouter = require('./routes/api');
 
 var app = express();
 
+app.use(cors());
 // view engine setup
+app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(logger('dev'));
 app.use(methodOverride('_method'));
 app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 }
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
 }));
 app.use(flash());
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -46,27 +47,24 @@ app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbo
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-//admin
+// admin
 app.use('/admin', adminRouter);
 app.use('/api/v1/member', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
-
-
-// token git : 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
